de276b178478d642874c5a7b065bad908948b097
Maksim Litvinov <lmk478shade@gmail.com>
2024-03-13 01:06:04 +0300

    Branch2 (#3)
    * feat: commit 5 branch2
    
    * feat: commit 6 branch2
    
    * feat: commit 7 branch2

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ console.log("commit 1 main");
 console.log("commit 2 main");
 console.log("commit 3 branch1");
 console.log("commit 4 branch1");
+
+console.log("commit 5 branch2");
+console.log("commit 6 branch2");
+console.log("commit 7 branch2");
